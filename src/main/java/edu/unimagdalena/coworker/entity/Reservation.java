6d4e3dc53892f71580025e39c050a473a286eadd
sebastian <sebsbl03@gmail.com>
2025-09-04 18:09:48 -0500

    base

---
+++ src/main/java/edu/unimagdalena/coworker/entity/Reservation.java
@@ -0,0 +1,34 @@
+package edu.unimagdalena.coworker.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "reservations")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private OffsetDateTime createdAt;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @OneToMany(mappedBy = "reservation")
+    private List<ReservationItem> items;
+
+    public void addItem(ReservationItem item) {
+        items.add(item);
+        item.setReservation(this);
+    }
+}
+
+
---
+++ src/main/java/edu/unimagdalena/coworker/entity/ReservationItem.java
@@ -0,0 +1,31 @@
+package edu.unimagdalena.coworker.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "reservation_items")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class ReservationItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private OffsetDateTime startAt;
+
+    @Column(nullable = false)
+    private OffsetDateTime endAt;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "reservation_id", nullable = false)
+    private Reservation reservation;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+}
+
